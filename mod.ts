e592daba958d182dfa0f4b2ebb2170508b61fe87
Hasin <hasinmahtab.alvee@gmail.com>
2024-05-23 21:39:26 +0600

    Updated methods

--- mod.ts
+++ mod.ts
@@ -4,11 +4,27 @@
  * @returns {string}
  * @example
  * ```
- * goodbye('Pansy') // => 'Goodbye Pansy 🔥'
+ * goodbyeConsole({ name: "hasin023" }); // Output: Goodbye hasin023 🔥
+ * ```
+ * This is a goodbye function. It says goodbye to the given name. It is a simple function that logs a string.
+ */
+
+export function goodbyeConsole({ name }: { name: string }) {
+	console.log(`Goodbye ${name || "World"} 🔥`);
+}
+
+/**
+ *
+ * @param {string} name
+ * @returns {string}
+ * @example
+ * ```
+ * const res = goodbyeReturn({ name: "hasin023" });
+ * console.log(res); // Output: Goodbye hasin023 🔥
  * ```
  * This is a goodbye function. It says goodbye to the given name. It is a simple function that returns a string.
  */
 
-export function goodbye({ name }: { name: string }) {
-	console.log(`Goodbye ${name || "World"} 🔥`);
+export function goodbyeReturn({ name }: { name: string }) {
+	return `Goodbye ${name || "World"} 🔥`;
 }
